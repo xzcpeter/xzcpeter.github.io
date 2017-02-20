03ab300bec4a70c66b2a408d02fc2c3ef2ecbf2e
小小dom <864731787@qq.com>
2017-02-20 11:39:14 +0800

    +
    +

---
+++ imooc/demo29.js
@@ -0,0 +1,17 @@
+/* myFocus.set({
+	id:'banner',
+	pattern:'mF_fancy',
+	time:3,
+	trigger:'mouseover',
+	width:1000,
+	height:310,
+}); */
+ myFocus.set({
+    id:'boxID',//焦点图盒子ID
+    pattern:'mF_fancy',//风格应用的名称
+    time:3,//切换时间间隔(秒)
+    trigger:'click',//触发切换模式:'click'(点击)/'mouseover'(悬停)
+    width:1000,//设置图片区域宽度(像素)
+    height:310,//设置图片区域高度(像素)
+    txtHeight:'default'//文字层高度设置(像素),'default'为默认高度，0为隐藏
+}); 
